fd1796c9f91f7c30a25403c1aeddeb537af786aa
Marcio R <Murcielago203@gmail.com>
2021-12-09 14:26:14 -0500

    final push added buttons ,

--- assets/single.js
+++ assets/single.js
@@ -2,27 +2,26 @@ var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
-
-
 var getRepoName = function() {
-    // grab repo name from url query string
-    var queryString = document.location.search;
-    var repoName = queryString.split("=")[1];
-    if(repoName) {
-        // display repo name on the page
-        repoNameEl.textContent = repoName;
-        getRepoIssues(repoName);
-    }
-    // if no repo was given, redirect to homepage
-    else {
-        document.location.replace("./index.html");
-    }
+  // grab repo name from url query string
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+
+  if (repoName) {
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+
+    getRepoIssues(repoName);
+  } else {
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
 };
+
 var getRepoIssues = function(repo) {
   // format the github api url
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
-//   getRepoName();
   // make a get request to url
   fetch(apiUrl).then(function(response) {
     // request was successful
@@ -35,10 +34,9 @@ var getRepoIssues = function(repo) {
           displayWarning(repo);
         }
       });
-    }
-    else {
-    // if not successful, redirect to homepage
-    document.location.replace("./index.html");
+    } else {
+      // if not successful, redirect to homepage
+      document.location.replace("./index.html");
     }
   });
 };
@@ -56,12 +54,11 @@ var displayIssues = function(issues) {
     issueEl.classList = "list-item flex-row justify-space-between align-center";
     issueEl.setAttribute("href", issues[i].html_url);
     issueEl.setAttribute("target", "_blank");
-    
+
     // create span to hold issue title
     var titleEl = document.createElement("span");
     titleEl.textContent = issues[i].title;
-    
-  
+
     // append to container
     issueEl.appendChild(titleEl);
 
@@ -71,14 +68,13 @@ var displayIssues = function(issues) {
     // check if issue is an actual issue or a pull request
     if (issues[i].pull_request) {
       typeEl.textContent = "(Pull request)";
-    }
-    else {
+    } else {
       typeEl.textContent = "(Issue)";
     }
 
     // append to container
     issueEl.appendChild(typeEl);
-  
+
     // append to the dom
     issueContainerEl.appendChild(issueEl);
   }
@@ -86,7 +82,7 @@ var displayIssues = function(issues) {
 
 var displayWarning = function(repo) {
   // add text to warning container
-  limitWarningEl.textContent = "To see more than 30 issues, visit";
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
 
   // create link element
   var linkEl = document.createElement("a");
